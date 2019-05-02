8cf377ca195d20ae82e5d679a426f5f32c56347f
ashwani23 <ashwanigoyal@outlook.in>
2019-05-02 18:26:23 +0530

    Adding files supporting the graphql setup

---
+++ models/Event.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var EventSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+});
+
+module.exports = mongoose.model('Event', EventSchema);
